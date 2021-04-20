de5d62a8e0ff9aa7ebaba6fd8b8492727fc768b1
Nicole Tribo <Nicoletribo@yahoo.com>
2021-04-19 22:16:39 -0500

    fixed my db

--- seed.sql
+++ seed.sql
@@ -3,25 +3,28 @@ CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
-CREATE TABLE department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+CREATE TABLE department (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE role(
-    id INT PRIMARY KEY,
+CREATE TABLE role (
+	id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id  INT NOT NULL
-    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id)
+    salary DECIMAL(12, 2),
+    department_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee(
-    id INT PRIMARY KEY,
+CREATE TABLE employee (
+	id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id)
-    manager_id INT
-    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id)
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
